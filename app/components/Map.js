ba7e9016cb6705e843eff7f4c47005b771a32a7d
Kenny Kim <kenny148@uw.edu>
2017-02-22 14:25:04 -0800

    Created Data Objects to Store Response

--- app/components/Map.js
+++ app/components/Map.js
@@ -10,7 +10,24 @@ import ScrollList from './ScrollList.js';
 const baseUrl = 'http://sentinelweb.9qqamtpp2r.us-east-1.elasticbeanstalk.com/getCrimeData.json';
 const crimeData = {
   simpleAssaultCrimes: [],
-  
+  motorVehicleTheftCrimes: [],
+  theftFromMotorVehicleCrimes: [],
+  burglaryBreakingAndEnteringCrimes:[],
+  counterfeitingForgeryCrimes: [],
+  robberyCrimes: [],
+  aggravatedAssaultCrimes: [],
+  prostitutionCrimes: [],
+  creditCardAndAtmFraudCrimes: [],
+  theftOfMotorVehiclePartsAndAccessoriesCrimes: [],
+  kidnappingAbductionCrimes: [],
+  impersonationCrimes: [],
+  shopliftingCrimes: [],
+  arsonCrimes: [],
+  drugNarcoticViolationCrimes: [],
+  destructionDmgVandOfPropertyCrimes: [],
+  theftFromBuildingCrimes: [],
+  falsePretensesSwindleConfidenceGameCrimes: [],
+  allOtherLarcenyCrimes: [],
 };
 
 class Map extends Component {
@@ -33,6 +50,25 @@ class Map extends Component {
     .then((responseJson) => {
       console.log(responseJson);
       this.state.crimeData.simpleAssaultCrimes = responseJson.simpleAssaultCrimes;
+      this.state.crimeData.motorVehicleTheftCrimes = responseJson.motorVehicleTheftCrimes;
+      this.state.crimeData.theftFromMotorVehicleCrimes = responseJson.theftFromMotorVehicleCrimes;
+      this.state.crimeData.burglaryBreakingAndEnteringCrimes = responseJson.burglaryBreakingAndEnteringCrimes;
+      this.state.crimeData.counterfeitingForgeryCrimes = responseJson.counterfeitingForgeryCrimes;
+      this.state.crimeData.robberyCrimes = responseJson.robberyCrimes;
+      this.state.crimeData.aggravatedAssaultCrimes = responseJson.aggravatedAssaultCrimes;
+      this.state.crimeData.prostitutionCrimes = responseJson.prostitutionCrimes;
+      this.state.crimeData.creditCardAndAtmFraudCrimes = responseJson.creditCardAndAtmFraudCrimes;
+      this.state.crimeData.theftOfMotorVehiclePartsAndAccessoriesCrimes = responseJson.theftOfMotorVehiclePartsAndAccessoriesCrimes;
+      this.state.crimeData.kidnappingAbductionCrimes = responseJson.kidnappingAbductionCrimes;
+      this.state.crimeData.impersonationCrimes = responseJson.impersonationCrimes;
+      this.state.crimeData.shopliftingCrimes = responseJson.shopliftingCrimes;
+      this.state.crimeData.arsonCrimes = responseJson.arsonCrimes;
+      this.state.crimeData.drugNarcoticViolationCrimes = responseJson.drugNarcoticViolationCrimes;
+      this.state.crimeData.destructionDmgVandOfPropertyCrimes = responseJson.destructionDmgVandOfPropertyCrimes;
+      this.state.crimeData.theftFromBuildingCrimes = responseJson.theftFromBuildingCrimes;
+      this.state.crimeData.falsePretensesSwindleConfidenceGameCrimes = responseJson.falsePretensesSwindleConfidenceGameCrimes;
+      this.state.crimeData.allOtherLarcenyCrimes = responseJson.allOtherLarcenyCrimes;
+
       console.log(this.state.crimeData);
     }).catch(function(error) {
     })
